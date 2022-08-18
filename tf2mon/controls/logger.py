fe20a411069060f314ebd123d19ca3e2e5fb11e6
Russel Lane <russel@rlane.com>
2022-08-18 15:34:23 -0500

    combine logger controls

--- tf2mon/controls/logger.py
+++ tf2mon/controls/logger.py
@@ -1,10 +1,35 @@
-"""Cycle logger `location` format."""
+"""Logger controls."""
 
 from enum import Enum
 
+from loguru import logger
+
 import tf2mon
-from tf2mon.control import CycleControl
+from tf2mon.control import Control, CycleControl
 from tf2mon.toggle import Toggle
+
+
+class LogLevelControl(CycleControl):
+    """Cycle logger `level`."""
+
+    name = "TOGGLE-LOG-LEVEL"
+    enum = Enum(f"_e_{name}", "INFO DEBUG TRACE")
+    toggle = Toggle(f"_t_{name}", enum)
+    items = {
+        enum.INFO: "INFO",  # ""
+        enum.DEBUG: "DEBUG",  # "-v"
+        enum.TRACE: "TRACE",  # "-vv"
+    }
+
+    def start(self) -> None:
+        """Set logging level based on `--verbose`."""
+
+        tf2mon.monitor.ui.logsink.set_verbose(tf2mon.monitor.options.verbose)
+        self.toggle.start(self.enum.__dict__[tf2mon.monitor.ui.logsink.level])
+
+    def handler(self, _match) -> None:
+        tf2mon.monitor.ui.logsink.set_level(self.items[self.toggle.cycle])
+        tf2mon.monitor.ui.show_status()
 
 
 class LogLocationControl(CycleControl):
@@ -42,3 +67,13 @@ class LogLocationControl(CycleControl):
         )
         self.add_fkey_to_help(arg)
         self.cli.add_default_to_help(arg)
+
+
+class ResetPaddingControl(Control):
+    """Reset logger `padding`."""
+
+    name = "RESET-PADDING"
+
+    def handler(self, _match) -> None:
+        tf2mon.monitor.ui.logsink.reset_padding()
+        logger.info("padding reset")
